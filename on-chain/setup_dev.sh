75b660a4f3f9c7efdc94ca4a36170bf80f63d00d
Shanmugavadivel Gopinath <gopinath@meridian-labs.io>
2025-08-01 17:28:46 +0530

    refactor: making functions readable

--- on-chain/setup_dev.sh
+++ on-chain/setup_dev.sh
@@ -3,10 +3,9 @@ set -e  # Exit immediately if any command fails
 
 mode=""
 amount=""
-restart_validator=false
-MINT_KEYPAIR_PATH="./.keys/2z_token_mint.json"
-MINT_AUTHORITY_KEYPAIR_PATH="./.keys/2z_token_mint_authority.json"
-PROTOCOL_TREASURY_KEYPAIR_PATH="./.keys/2z_protocol_treasury.json"
+DOUBLE_ZERO_MINT_KEYPAIR_PATH="./.keys/2z_token_mint.json"
+DOUBLE_ZERO_MINT_AUTHORITY_KEYPAIR_PATH="./.keys/2z_token_mint_authority.json"
+DOUBLE_ZERO_PROTOCOL_WALLET_KEYPAIR_PATH="./.keys/2z_protocol_treasury.json"
 PROTOCOL_TREASURY_TOKEN_ACCOUNT_KEYPAIR_PATH="./.keys/2z_protocol_treasury_token_account.json"
 
 GREEN='\033[0;32m'
@@ -44,12 +43,13 @@ show_help() {
   echo "Setup Dev script for Converter Program"
   echo ""
   echo "OPTIONS:"
-  echo "  -m, --mode Mode      Set the mode of operation ('setup_mint', 'setup_ata', or 'mint')."
+  echo "  -m, --mode Mode      Set the mode of operation ('setup_double_zero_accounts', 'setup_user_ata', or 'mint_to_user_ata'."
   echo "  -a, --amount Amount      Set the amount for mint operation."
   echo "  -h, --help          Show this help message"
   echo ""
   echo "Example:"
-  echo "  ./build_and_deploy.sh --mode build_and_deploy --restart-validator"
+  echo "./setup_dev.sh --m setup_double_zero_accounts"
+  echo "./setup_dev.sh --m mint_to_user_ata -a 50"
 }
 
 generate_key_pair() {
@@ -67,35 +67,35 @@ generate_key_pair() {
   log_success "Public Key of $1: $PUBKEY"
 }
 
-setup_mint_and_token_account() {
+setup_2z_mint_and_vault_and_protocol_treasury_account() {
   log_section "Token Setup"
   # setting to local net
   solana config set --url localhost
-  generate_key_pair "2Z Protocol Treasury wallet" $PROTOCOL_TREASURY_KEYPAIR_PATH
-  generate_key_pair "2Z Token Mint" $MINT_KEYPAIR_PATH
-  generate_key_pair "2Z Token Mint Authority" $MINT_AUTHORITY_KEYPAIR_PATH
+  generate_key_pair "2Z Protocol wallet" $DOUBLE_ZERO_PROTOCOL_WALLET_KEYPAIR_PATH
+  generate_key_pair "2Z Token Mint" $DOUBLE_ZERO_MINT_KEYPAIR_PATH
+  generate_key_pair "2Z Token Mint Authority" $DOUBLE_ZERO_MINT_AUTHORITY_KEYPAIR_PATH
   generate_key_pair "2Z Protocol Treasury Token Account" $PROTOCOL_TREASURY_TOKEN_ACCOUNT_KEYPAIR_PATH
 
-  MINT_ADDRESS=$(solana-keygen pubkey "$MINT_KEYPAIR_PATH")
-  PROTOCOL_TREASURY_ADDRESS=$(solana-keygen pubkey "$PROTOCOL_TREASURY_KEYPAIR_PATH")
+  MINT_ADDRESS=$(solana-keygen pubkey "$DOUBLE_ZERO_MINT_KEYPAIR_PATH")
+  DOUBLE_ZERO_PROTOCOL_WALLET_ADDRESS=$(solana-keygen pubkey "$DOUBLE_ZERO_PROTOCOL_WALLET_KEYPAIR_PATH")
   PROTOCOL_TREASURY_TOKEN_ACCOUNT_ADDRESS=$(solana-keygen pubkey "$PROTOCOL_TREASURY_TOKEN_ACCOUNT_KEYPAIR_PATH")
 
-  solana airdrop 500 "$PROTOCOL_TREASURY_ADDRESS" > /dev/null
+  solana airdrop 500 "$DOUBLE_ZERO_PROTOCOL_WALLET_ADDRESS" > /dev/null
 
   # Check if mint account already exists
   if solana account "$MINT_ADDRESS" > /dev/null 2>&1; then
     log_info "2Z Mint already exists at $MINT_ADDRESS. Skipping creation."
   else
     log_info "2Z Mint not found. Creating new token mint..."
-    spl-token --program-2022 create-token $MINT_KEYPAIR_PATH --mint-authority "$MINT_AUTHORITY_KEYPAIR_PATH"> /dev/null
-    log_success "2Z Mint is created at $MINT_KEYPAIR_PATH."
+    spl-token --program-2022 create-token $DOUBLE_ZERO_MINT_KEYPAIR_PATH --mint-authority "$DOUBLE_ZERO_MINT_AUTHORITY_KEYPAIR_PATH"> /dev/null
+    log_success "2Z Mint is created at $DOUBLE_ZERO_MINT_KEYPAIR_PATH."
   fi
 
   # Check if Protocol Treasury Account exists or not
   if solana account "$PROTOCOL_TREASURY_TOKEN_ACCOUNT_ADDRESS" > /dev/null 2>&1; then
-    log_info "ATA already exists at $PROTOCOL_TREASURY_TOKEN_ACCOUNT_ADDRESS. Skipping creation."
+    log_info "Protocol Treasury ATA already exists at $PROTOCOL_TREASURY_TOKEN_ACCOUNT_ADDRESS. Skipping creation."
   else
-    log_info "ATA not found. Creating new token account..."
+    log_info "Protocol Treasury ATA not found. Creating new token account..."
     spl-token --program-2022 create-account "$MINT_ADDRESS" "$PROTOCOL_TREASURY_TOKEN_ACCOUNT_KEYPAIR_PATH"  > /dev/null
     log_success "Protocol Treasury ATA successfully created at $PROTOCOL_TREASURY_TOKEN_ACCOUNT_ADDRESS."
   fi
@@ -103,20 +103,19 @@ setup_mint_and_token_account() {
   TOKEN_BALANCE=$(spl-token balance --address "$PROTOCOL_TREASURY_TOKEN_ACCOUNT_ADDRESS" | tr -d '\n')
   if [ "$TOKEN_BALANCE" -lt 500 ]; then
     log_info "💸 Minting 500 2Z tokens to the treasury..."
-    spl-token mint "$MINT_ADDRESS" 500 "$PROTOCOL_TREASURY_TOKEN_ACCOUNT_ADDRESS" --mint-authority "$MINT_AUTHORITY_KEYPAIR_PATH" > /dev/null
+    spl-token mint "$MINT_ADDRESS" 500 "$PROTOCOL_TREASURY_TOKEN_ACCOUNT_ADDRESS" --mint-authority "$DOUBLE_ZERO_MINT_AUTHORITY_KEYPAIR_PATH" > /dev/null
     TOKEN_BALANCE=$(spl-token balance --address "$PROTOCOL_TREASURY_TOKEN_ACCOUNT_ADDRESS" | tr -d '\n')
   fi
   log_info "TOKEN BALANCE OF 2Z PROTOCOL TREASURY $TOKEN_BALANCE"
 
   log_success "2Z Token Setup Completed"
   log_info "🧾 Mint Address         : $MINT_ADDRESS"
-  log_info "💼 Treasury Wallet      : $PROTOCOL_TREASURY_ADDRESS"
+  log_info "💼 Double Zero Protocol Wallet      : $DOUBLE_ZERO_PROTOCOL_WALLET_ADDRESS"
   log_info "🏦 Token Account (2Z Protocol Treasury): $PROTOCOL_TREASURY_TOKEN_ACCOUNT_ADDRESS"
 }
 
-setup_user_token_account() {
-  MINT_KEYPAIR_PATH="./.keys/2z_token_mint.json"
-  MINT_ADDRESS=$(solana-keygen pubkey "$MINT_KEYPAIR_PATH")
+setup_user_2Z_associated_token_account() {
+  MINT_ADDRESS=$(solana-keygen pubkey "$DOUBLE_ZERO_MINT_KEYPAIR_PATH")
   ATA_ADDRESS=$(spl-token address --token "$MINT_ADDRESS" --verbose | awk '/Associated token address:/ { print $NF }')
 
   # Check if ATA exists or not
@@ -129,15 +128,17 @@ setup_user_token_account() {
   fi
 }
 
-mint_user_ata_account() {
-  MINT_ADDRESS=$(solana-keygen pubkey "$MINT_KEYPAIR_PATH")
+mint_to_user_2Z_ata_account() {
+  MINT_ADDRESS=$(solana-keygen pubkey "$DOUBLE_ZERO_MINT_KEYPAIR_PATH")
   ATA_ADDRESS=$(spl-token address --token "$MINT_ADDRESS" --verbose | awk '/Associated token address:/ { print $NF }')
   if [[ -z "$amount" ]]; then
     log_warning "No mint amount specified. Defaulting to 5 2Z Tokens"
     amount="5"
   fi
-  spl-token mint "$MINT_ADDRESS" "$amount" "$ATA_ADDRESS" --mint-authority "$MINT_AUTHORITY_KEYPAIR_PATH"
+  spl-token mint "$MINT_ADDRESS" "$amount" "$ATA_ADDRESS" --mint-authority "$DOUBLE_ZERO_MINT_AUTHORITY_KEYPAIR_PATH"
   log_success "Minted $amount Tokens to $ATA_ADDRESS"
+  TOKEN_BALANCE=$(spl-token balance --address "$ATA_ADDRESS" | tr -d '\n')
+  log_info "TOKEN BALANCE OF USER $TOKEN_BALANCE"
 }
 
 cd "$(dirname "$0")" || exit 1
@@ -169,18 +170,18 @@ while [[ $# -gt 0 ]]; do
 done
 
 # Handle modes
-case "$mode" in
-  setup_mint)
-    setup_mint_and_token_account
+case "$mode" in 
+  setup_double_zero_accounts)
+    setup_2z_mint_and_vault_and_protocol_treasury_account
     ;;
-  setup_ata)
-    setup_user_token_account
+  setup_user_ata)
+    setup_user_2Z_associated_token_account
     ;;
-  mint)
-    mint_user_ata_account
+  mint_to_user_ata)
+    mint_to_user_2Z_ata_account
     ;;
   *)
-    log_error "Invalid mode specified. Please use 'setup_mint', 'setup_ata', or 'mint'."
+    log_error "Invalid mode specified. Please use 'setup_double_zero_accounts', 'setup_user_ata', or 'mint_to_user_ata'."
     exit 1
     ;;
 esac
