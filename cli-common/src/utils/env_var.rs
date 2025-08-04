e2d6ed8b976dd14893f8114be2519bc6f1ef4512
Shanmugavadivel Gopinath <gopinath@meridian-labs.io>
2025-08-04 16:37:42 +0530

    fix: Fixing merge conflicts

--- cli-common/src/utils/env_var.rs
+++ cli-common/src/utils/env_var.rs
@@ -30,8 +30,8 @@ pub fn load_private_key() -> Result<Vec<u8>, Box<dyn Error>> {
 
 pub fn load_payer_from_env() -> Result<Keypair, Box<dyn Error>> {
     let private_key = load_private_key()?;
-    let payer = load_payer_from_env()?;
-    // above line is deprecated from 2.2.0. below line is the fix. skipping this for older version compatibility
+    let payer = Keypair::from_bytes(&private_key)?;
+    // above line is deprecated from solana 2.2.0. below line is the fix. skipping this for older version compatibility
     // let payer = Keypair::try_from(&private_key[..])?;
     Ok(payer)
 }
