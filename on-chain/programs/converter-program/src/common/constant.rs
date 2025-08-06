41fd2e7d729dcfd74194dd2c4c40d5f8625a625d
Shanmugavadivel Gopinath <gopinath@meridian-labs.io>
2025-08-06 18:09:47 +0530

    feature: Completing on-chain implementation for buy sol

--- on-chain/programs/converter-program/src/common/constant.rs
+++ on-chain/programs/converter-program/src/common/constant.rs
@@ -10,3 +10,4 @@ pub const DECIMAL_PRECISION: u64 = 10000;
 
 // Account Size
 pub const DISCRIMINATOR_SIZE: usize = 8;
+pub const TOKEN_DECIMALS: u64 = 6;
--- on-chain/programs/converter-program/src/user_flow/buy_sol.rs
+++ on-chain/programs/converter-program/src/user_flow/buy_sol.rs
@@ -27,6 +27,7 @@ use mock_transfer_program::{
     },
     program::MockTransferProgram
 };
+use crate::common::constant::TOKEN_DECIMALS;
 
 #[derive(Accounts)]
 pub struct BuySol<'info> {
@@ -101,13 +102,16 @@ impl<'info> BuySol<'info> {
         
         let sol_quantity = self.configuration_registry.sol_quantity;
         // call util function to get current ask price
-        let ask_price = calculate_ask_price_with_oracle_price_data(
+        let raw_ask_price = calculate_ask_price_with_oracle_price_data(
             &self.program_state.trade_history_list,
             sol_quantity,
             self.configuration_registry.steepness,
             self.configuration_registry.max_discount_rate,
             oracle_price_data
         )?;
+
+        let ask_price = raw_ask_price.checked_mul(TOKEN_DECIMALS)
+            .ok_or(DoubleZeroError::ArithmeticError)?;
 
         let clock = Clock::get()?;
         
@@ -124,10 +128,12 @@ impl<'info> BuySol<'info> {
             return err!(DoubleZeroError::BidTooLow);
         }
         
-        let tokens_required = sol_quantity.checked_mul(bid_price)
-            .ok_or(DoubleZeroError::ArithmeticError)?
-            .checked_div(LAMPORTS_PER_SOL)
-            .ok_or(DoubleZeroError::ArithmeticError)?;
+        // let tokens_required = sol_quantity.checked_mul(bid_price)
+        //     .ok_or(DoubleZeroError::ArithmeticError)?
+        //     .checked_div(LAMPORTS_PER_SOL)
+        //     .ok_or(DoubleZeroError::ArithmeticError)?;
+
+        let tokens_required = bid_price;
 
         let cpi_accounts = BuySolCpi {
             vault_account: self.vault_account.to_account_info(),
