45dd7c916428b665453724d60fe3c09b04fe4b2d
Dinushka Samaranayake <dinushka.samaranayake@yaalalabs.com>
2025-08-07 12:41:16 +0530

    separate conversion price and ask price

--- on-chain/programs/converter-program/src/lib.rs
+++ on-chain/programs/converter-program/src/lib.rs
@@ -103,10 +103,10 @@ pub mod converter_program {
             .process(bid_price, swap_rate, timestamp, attestation)
     }
 
-    pub fn calculate_ask_price(
+    pub fn get_conversion_rate(
         ctx: Context<CalculateAskPrice>,
         oracle_price_data: OraclePriceData,
     ) -> Result<u64> {
-        ctx.accounts.process(oracle_price_data)
+        ctx.accounts.get_conversion_rate(oracle_price_data)
     }
 }
