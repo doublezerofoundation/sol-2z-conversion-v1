699f315b1b7a9ed04ec9e690346c4b8fcf1d4229
buddhini <buddhini@yaalalabs.com>
2025-08-12 13:44:42 +0530

    DOUB-46 Add tests + mocks

---
+++ off-chain/indexer-service/tests/mock/config-util.ts
@@ -0,0 +1,8 @@
+// Mock for: src/utils/config.ts
+const cfg = {
+  RPC_URL: 'http://localhost:8899',
+  PROGRAM_ID: '11111111111111111111111111111111',
+  CONCURRENCY: 4,
+};
+export const ENV = 'test';
+export default cfg;
