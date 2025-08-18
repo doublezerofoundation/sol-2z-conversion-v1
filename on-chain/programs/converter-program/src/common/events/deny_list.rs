0e18e214db873c160fb28072d290581bc8544beb
buddhini <buddhini@yaalalabs.com>
2025-08-18 15:48:39 +0530

    [on-chain] Add deny_list events

---
+++ on-chain/programs/converter-program/src/common/events/deny_list.rs
@@ -0,0 +1,16 @@
+
+#[event]
+pub struct DenyListAddressAdded {
+    pub added_by: Pubkey,
+    pub address: Pubkey,
+    pub timestamp: i64,
+    pub update_count: u64,
+}
+
+#[event]
+pub struct DenyListAddressRemoved {
+    pub removed_by: Pubkey,
+    pub address: Pubkey,
+    pub timestamp: i64,
+    pub update_count: u64,
+}
