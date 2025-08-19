4291414d2836898e387b9d84271c342ab86b8ed2
Shanmugavadivel Gopinath <gopinath@meridian-labs.io>
2025-08-19 12:13:25 +0530

    fix: idl not includes the errorcodes
    Signed-off-by: Shanmugavadivel Gopinath <gopinath@meridian-labs.io>

--- on-chain/programs/converter-program/src/fills_registry/queue_implementation.rs
+++ on-chain/programs/converter-program/src/fills_registry/queue_implementation.rs
@@ -1,23 +1,19 @@
 use crate::{
     fills_registry::fills_registry::{FillsRegistry, Fill},
-    common::constant::MAX_FILLS_QUEUE_SIZE
+    common::{
+        constant::MAX_FILLS_QUEUE_SIZE,
+        error::DoubleZeroError
+    }
 };
 use anchor_lang::prelude::*;
 
-#[error_code]
-pub enum FillRegistryError {
-    #[msg("FillsRegistry is full — cannot enqueue.")]
-    RegistryFull,
-    #[msg("FillsRegistry is empty — cannot dequeue.")]
-    RegistryEmpty,
-}
 impl FillsRegistry {
 
     // Data Structure Implementation - Solana does not have implementation for queue
     /// Adds it to Queue
     pub fn enqueue(&mut self, fill: Fill) -> Result<()> {
         if self.count as usize >= MAX_FILLS_QUEUE_SIZE {
-            return err!(FillRegistryError::RegistryFull);
+            return err!(DoubleZeroError::RegistryFull);
         }
         self.fills[self.tail as usize] = fill;
         self.tail = (self.tail + 1) % MAX_FILLS_QUEUE_SIZE as u64;
@@ -27,7 +23,7 @@ impl FillsRegistry {
 
     /// Remove & Return the old entry - FIFO
     pub fn dequeue(&mut self) -> Result<Fill> {
-        require!(!self.is_empty(), FillRegistryError::RegistryEmpty);
+        require!(!self.is_empty(), DoubleZeroError::RegistryEmpty);
         let fill = self.fills[self.head as usize]; // copy the Fill (Fill is Copy)
         self.head = (self.head + 1) % MAX_FILLS_QUEUE_SIZE as u64;
         self.count -= 1;
@@ -36,7 +32,7 @@ impl FillsRegistry {
 
     /// Return the old entry - FIFO : Does Not Remove
     pub fn peek(&self) -> Result<&Fill> {
-        require!(!self.is_empty(), FillRegistryError::RegistryEmpty);
+        require!(!self.is_empty(), DoubleZeroError::RegistryEmpty);
         let fill = &self.fills[self.head as usize];
         Ok(fill)
     }
