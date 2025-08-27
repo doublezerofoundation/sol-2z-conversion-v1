3a6a53379230494a573bee9d130c57b4bfdfce7e
Dinushka Samaranayake <dinushka.samaranayake@yaalalabs.com>
2025-08-27 13:48:43 +0530

    chore: update e2e token decimals to 8

--- e2e/src/core/constants.ts
+++ e2e/src/core/constants.ts
@@ -3,4 +3,4 @@ export const DEFAULT_RPC_URL = "http://localhost:8899";
 
 export const PRICE_ORACLE_END_POINT = "https://ls28awygrk.execute-api.us-east-1.amazonaws.com/dev1/api/v1/swap-rate";
 
-export const TOKEN_DECIMALS = 1000000;
+export const TOKEN_DECIMALS = 100000000;
