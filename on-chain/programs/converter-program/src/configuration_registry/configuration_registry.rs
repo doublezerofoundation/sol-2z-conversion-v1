f68d87b7953a97949a88eb7fedaf097256232106
Dinushka Samaranayake <dinushka.samaranayake@yaalalabs.com>
2025-08-29 11:34:05 +0530

    fix: update comment to correctly show the coefficient limit

--- on-chain/programs/converter-program/src/configuration_registry/configuration_registry.rs
+++ on-chain/programs/converter-program/src/configuration_registry/configuration_registry.rs
@@ -8,7 +8,7 @@ pub struct ConfigurationRegistry {
     pub price_maximum_age: i64, // Maximum acceptable age for oracle price data
     pub fills_consumer: Pubkey,
     // Price calculation
-    pub coefficient: u64, // Coefficient of the discount function in basis points (0 <= coefficient <= 10_000)
+    pub coefficient: u64, // Coefficient of the discount function in basis points (0 <= coefficient <= 100_000_000)
     pub max_discount_rate: u64, // Maximum discount rate in basis points (0 <= max_discount_rate <= 10_000)
     pub min_discount_rate: u64, // Minimum discount rate in basis points (0 <= min_discount_rate <= 10_000)
 }
