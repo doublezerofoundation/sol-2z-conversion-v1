c44d329efc06b950dc91805c4dd06eaa69b6e087
Kaanathas Sivarajah <kaanathas@meridian-labs.io>
2025-08-29 13:02:25 +0530

    enable proxy intergration to pricing service api

--- deployment/modules/api_gateway/main.tf
+++ deployment/modules/api_gateway/main.tf
@@ -144,6 +144,7 @@ resource "aws_api_gateway_deployment" "this" {
   rest_api_id = aws_api_gateway_rest_api.this.id
 
   triggers = {
+    force_redeploy = timestamp()
     # NOTE: The configuration below will satisfy ordering considerations,
     # but not pick up all future REST API changes. More advanced patterns
     # are possible, such as using the filesha1() function against the
