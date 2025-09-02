35926dd113aee5fbe406038a97c1b062d3e809ed
Shanmugavadivel Gopinath <gopinath@meridian-labs.io>
2025-09-02 18:05:52 +0530

    fix: copilot-pr review
    Signed-off-by: Shanmugavadivel Gopinath <gopinath@meridian-labs.io>

--- cli-common/src/utils/fixed_point_utils.rs
+++ cli-common/src/utils/fixed_point_utils.rs
@@ -33,7 +33,7 @@ pub fn convert_sol_value(sol_amount: u64) -> Decimal {
 }
 
 pub fn convert_token_value(token_amount: u64) -> Decimal {
-    let with_decimals = Decimal::from_u64(token_amount).expect("Invalid sol value");
+    let with_decimals = Decimal::from_u64(token_amount).expect("Invalid token value");
     let token_val = Decimal::from_u64(TOKEN_UNITS).unwrap();
     with_decimals / token_val
 }
