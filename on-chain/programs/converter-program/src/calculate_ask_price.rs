25059be0dfc4a56dc2dd521be7f2bbbe4caacf77
Shanmugavadivel Gopinath <gopinath@meridian-labs.io>
2025-09-22 21:05:28 +0530

    fix: removing migration instruction & removing denylist from calculate Price
    Signed-off-by: Shanmugavadivel Gopinath <gopinath@meridian-labs.io>

--- on-chain/programs/converter-program/src/calculate_ask_price.rs
+++ on-chain/programs/converter-program/src/calculate_ask_price.rs
@@ -20,7 +20,6 @@ use crate::{
         structs::OraclePriceData, attestation_utils::verify_attestation,
     },
     configuration_registry::configuration_registry::ConfigurationRegistry,
-    deny_list_registry::DenyListRegistry,
     program_state::ProgramStateAccount,
 };
 
@@ -36,12 +35,7 @@ pub struct CalculateAskPrice<'info> {
         seeds = [SeedPrefixes::ConfigurationRegistry.as_bytes()],
         bump = program_state.bump_registry.configuration_registry_bump,
     )]
-    pub configuration_registry: Account<'info, ConfigurationRegistry>,
-    #[account(
-        seeds = [SeedPrefixes::DenyListRegistry.as_bytes()],
-        bump = program_state.bump_registry.deny_list_registry_bump,
-    )]
-    pub deny_list_registry: Account<'info, DenyListRegistry>,
+    pub configuration_registry: Account<'info, ConfigurationRegistry>
 }
 
 impl<'info> CalculateAskPrice<'info> {
--- on-chain/programs/converter-program/src/lib.rs
+++ on-chain/programs/converter-program/src/lib.rs
@@ -8,7 +8,6 @@ mod fills_registry;
 mod init_system;
 mod program_state;
 mod buy_sol;
-mod migration;
 mod deny_list_registry;
 
 use system_management::set_admin::*;
@@ -24,10 +23,6 @@ use buy_sol::*;
 use deny_list_registry::*;
 use fills_registry::dequeue_fills::*;
 use fills_registry::fills_registry::*;
-#[cfg(feature = "test")]
-use migration::migrate_v1_to_v2::*;
-#[cfg(feature = "test")]
-use migration::rollback_v2_to_v1::*;
 
 declare_id!("YrQk4TE5Bi6Hsi4u2LbBNwjZUWEaSUaCDJdapJbCE4z");
 #[program]
@@ -121,27 +116,5 @@ pub mod converter_program {
         max_sol_amount: u64,
     ) -> Result<DequeueFillsResult> {
         ctx.accounts.process(max_sol_amount)
-    }
-
-    //////////////////////// Example Migration ////////////////////////
-    //////////////////////// !!! Only as an example ////////////////////////
-    #[cfg(feature = "test")]
-    pub fn migrate_v1_to_v2(
-        ctx: Context<MigrateV1ToV2>,
-    ) -> Result<()> {
-        ctx.accounts.process(
-            ctx.bumps.configuration_registry_new,
-            ctx.bumps.deny_list_registry_new
-        )
-    }
-
-    #[cfg(feature = "test")]
-    pub fn rollback_v2_to_v1(
-        ctx: Context<RollbackV2toV1>,
-    ) -> Result<()> {
-        ctx.accounts.process(
-            ctx.bumps.configuration_registry_new,
-            ctx.bumps.deny_list_registry_new
-        )
     }
 }
