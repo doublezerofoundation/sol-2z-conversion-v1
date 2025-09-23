56539bca327049aa1721daeb98bc047820c2815c
Shanmugavadivel Gopinath <gopinath@meridian-labs.io>
2025-09-23 13:11:58 +0530

    fix: removing migration instruction & removing denylist from calculate Price
    Signed-off-by: Shanmugavadivel Gopinath <gopinath@meridian-labs.io>

--- on-chain/programs/converter-program/src/calculate_ask_price.rs
+++ on-chain/programs/converter-program/src/calculate_ask_price.rs
@@ -20,7 +20,6 @@ use crate::{
         structs::OraclePriceData, attestation_utils::verify_attestation,
     },
     configuration_registry::configuration_registry::ConfigurationRegistry,
-    deny_list_registry::DenyListRegistry,
     program_state::ProgramStateAccount,
 };
 
@@ -36,12 +35,7 @@ pub struct CalculateAskPrice<'info> {
         seeds = [seeds::CONFIGURATION_REGISTRY],
         bump = program_state.bump_registry.configuration_registry_bump,
     )]
-    pub configuration_registry: Account<'info, ConfigurationRegistry>,
-    #[account(
-        seeds = [seeds::DENY_LIST_REGISTRY],
-        bump = program_state.bump_registry.deny_list_registry_bump,
-    )]
-    pub deny_list_registry: Account<'info, DenyListRegistry>,
+    pub configuration_registry: Account<'info, ConfigurationRegistry>
 }
 
 impl<'info> CalculateAskPrice<'info> {
